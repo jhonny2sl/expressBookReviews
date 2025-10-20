e8a38c446aa786ff7064bf83cec26bc8ffbf552a
jhonny2sl <jhonnytwosl@gmail.com>
2025-10-20 10:32:02 +0700

    Added router to get book details based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -15,21 +15,38 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    // Send JSON response with formatted books data
-    res.send(JSON.stringify(books, null, 4));
+  // Send JSON response with formatted books data
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    // Retrieve the isbn parameter from the request URL and send the corresponding book's details
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
+  // Retrieve the isbn parameter from the request URL and send the corresponding book's details
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the author parameter from the request URL
+  const author = req.params.author.trim().toLowerCase();
+
+  const results = {};
+
+  // Iterate through the ‘books’ array & check the author matches the one provided in the request parameter
+  for (const isbn in books) {
+    if (books[isbn].author.toLowerCase() === author) {
+      results[isbn] = books[isbn];
+    }
+  }
+
+  // If no books found for the author, return 404
+  if (Object.keys(results).length === 0) {
+    return res.status(404).json({ message: "No books found for that author" });
+  }
+
+  // Send the results as the response
+  res.send(results);
 });
 
 // Get all books based on title
